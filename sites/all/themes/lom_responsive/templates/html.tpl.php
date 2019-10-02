5032c29f44ce38844fdecd0bf5c7549257ffb9a7
Ranto Andrianavonison <ranto@tyarai.com>
2019-10-02 08:44:51 +0300

    Tring different header

---
+++ sites/all/themes/lom_responsive/templates/html.tpl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+<?php print $head; ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title><?php print $head_title; ?></title>
+<!-- Bootstrap CSS -->
+<?php print $styles; ?>
+<?php print $scripts; ?>
+<link 
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
