b516cc46ced50d21450f7d45cfcebd3e4df80525
Ranto Andrianavonison <ranto@tyarai.com>
2019-10-02 13:26:04 +0300

    Finished adding bootstrap

---
+++ sites/all/themes/lom_responsive/templates/navbar.tpl.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light header-bg">
+    <div class="container">
+      <a class="navbar-brand" href="/"><img src="/sites/default/files/logo.png" alt="Lemurs Of Madagascar" class="img-fluid" width="100" ></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+
+
+          <li class="nav-item">
+            <a class="nav-link" href="<?php print $base_url .'/sightings'; ?>">My sightings</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php print $base_url .'/lifelist';?>">My lemur life list</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php print $base_url .'/sighting/add';?>">New sighting</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php print $base_url .'/public_sightings';?>">Public sightings</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+
+    </div>
+</nav>  
+
