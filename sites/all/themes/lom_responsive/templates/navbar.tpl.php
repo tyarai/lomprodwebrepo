12619689f205e54b54ddac62edd03cac8b91bab2
Ranto Andrianavonison <ranto@tyarai.com>
2019-10-29 14:08:57 +0300

    -

--- sites/all/themes/lom_responsive/templates/navbar.tpl.php
+++ sites/all/themes/lom_responsive/templates/navbar.tpl.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light header-bg">
     <div class="container">
-      <a class="navbar-brand" href="/"><img src="/sites/default/files/logo.png" alt="Lemurs Of Madagascar" class="img-fluid" width="100" ></a>
+      <a class="navbar-brand" href="<?php print $base_path;?>"><img src="<?php print $base_path;?>sites/default/files/logo.png" alt="Lemurs Of Madagascar" class="img-fluid" width="100" ></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
